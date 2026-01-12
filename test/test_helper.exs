dab2daddf9f29d1babb4356d4d89a506b033f538
kantum <42kantum@gmail.com>
2026-01-12 12:11:01 +0100

    fix: move filter override to support directory
    add SPDX license identifiers

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,7 +4,4 @@
 
 ExUnit.start()
 
-# Ensure the filter override module is loaded before tests
-Code.require_file(Path.join(__DIR__, "filter_override.exs"))
-
 AshAuthentication.Phoenix.Test.Endpoint.start_link()
